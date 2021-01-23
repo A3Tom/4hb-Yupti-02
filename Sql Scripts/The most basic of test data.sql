cbfaca8ecb08c9bc7d10595acfb880ab7bbb0f6d
A3Tom <Tom_Esq@Yahoo.com>
2021-01-23 18:56:09 +0000

    Updated lists table to Grouping table as it was fuckin confusing

--- Sql Scripts/The most basic of test data.sql
+++ Sql Scripts/The most basic of test data.sql
@@ -4,14 +4,14 @@ VALUES
 	('Test Boards	', 'This is first test board', 1)
 ;
 
-INSERT INTO Lists
+INSERT INTO Groupings
 	(BoardId, Title, Description, Labels, Archived, Completed)
 VALUES
-	(1, 'List 1', 'A real OG', 'OG', 0, 0)
+	(1, 'Grouping 1', 'A real OG', 'OG', 0, 0)
 ;
 
 INSERT INTO Cards
-	(ListId, Title, Description, Labels, Archived, Completed)
+	(GroupingId, Title, Description, Labels, Archived, Completed)
 VALUES
 	(1, 'Card 1', 'The first card to exist', 'First card, Hing, Frogs', 0, 0)
 ;
